4ead3ceaf0baaa2cffa5350ac21679693aa6fe42
Hien <Hien@192.168.101.9>
2024-06-27 09:41:24 +0700

    commit for lab2

---
+++ labs/lab2.js
@@ -0,0 +1,25 @@
+//Lab 2.1
+const readline = require('readline-sync');
+let yourWeight = readline.question('Your weight: ');
+let yourHeight = readline.question('Your height: ');
+let yourBMI = yourWeight / (yourHeight*2);
+if(yourBMI <= 18.5) {
+    console.log('Underweight');
+} else if (yourBMI > 18.5 && yourBMI <=24.9) {
+    console.log('Normal weight');
+} else if(yourBMI >= 25 && yourBMI <=29.9) {
+    console.log('Over weight ');
+} else {
+    console.log('Obesity');
+};
+
+// Lab 2.2
+/*const readline = require('readline-sync');
+let inputNumber = readline.question('Number: ')
+let result = inputNumber % 2
+if(result==0) {
+    console.log ("It's an even number");
+} else {
+    console.log("It's an odd number");
+};
+*/
